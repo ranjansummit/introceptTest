7fea1f00bf5a915bb9e0b5957fc22fd23e241f62
ranjandhungana <ranjan.summit@gmai.com>
2019-11-09 20:00:15 +0545

    table implemented

--- app/component_scenes/Home.js
+++ app/component_scenes/Home.js
@@ -1,27 +1,57 @@
 
-import React, {Component} from 'react';
-import {StyleSheet, 
-   Text,
-   View,
-   ScrollView,
-   TouchableWithoutFeedback,
-   Keyboard,
-   TextInput,
-   Image,
-   TouchableOpacity
-  } 
-from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  Alert
+}
+  from 'react-native';
 
-import {connect} from 'react-redux';
+  import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
+import { connect } from 'react-redux';
 
 import Theme from '../uielements/Utility/Colors';
+ class Home extends Component {
+ 
 
-class Home extends Component {
   constructor(props) {
     super(props);
+    const elementButton = (value) => (
+      <TouchableOpacity onPress={() => this._alertIndex(value)}>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>button</Text>
+        </View>
+      </TouchableOpacity>
+    );
+
     this.state = {
       name: '',
       email: '',
+      tableTitle: ['Title', 'Title2', 'Title3', 'Title4','Title', 'Title2', 'Title3', 'Title4','Title', 'Title2', 'Title3', 'Title4','Title', 'Title2', 'Title3', 'Title4','Title', 'Title2', 'Title3', 'Title4','Title', 'Title2', 'Title3', 'Title4'],
+      tableData: [
+        [elementButton('1'), 'a', 'b', 'c', 'd'],
+        [elementButton('2'), '1', '2', '3', '4'],
+        [elementButton('3'), 'a', 'b', 'c', 'd']
+      ],
+
+      value: [
+        { key: "1",width:33, title: "name" },
+        { key: "2",width:33, title: "mobileno",sortable: true  },
+        { key: "3",width:33, title: "mobilebrand" ,sortable: true },
+
+      ],
+      field: [
+        { name:"ranjan",mobileno:"9840051137",mobilebrand:"nokia"},
+        { name:"ranjan",mobileno:"9840051137",mobilebrand:"nokia"},  
+       { name:"ranjan",mobileno:"9840051137",mobilebrand:"nokia"}
+      ],
       mobile_number: '',
       password: '',
       confirm_password: '',
@@ -34,215 +64,249 @@ class Home extends Component {
       password_error: '',
       confirm_password_error: '',
     };
-    // this.emailAlert = this.emailAlert.bind(this);
-    // this.mobileAlert = this.mobileAlert.bind(this);
   }
+  _alertIndex(value) {
+    Alert.alert(`This is column ${value}`);
+  }
+
+
   render() {
     return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <ScrollView>
-        {this.props.loading ? <Spinner /> : null}
-        {this.props.error ? (
-          this.props.error.email ? (
-            <View>{this.emailAlert()}</View>
-          ) : this.props.error.mobile ? (
-            <View>{this.mobileAlert()}</View>
-          ) : null
-        ) : null}
-        {this.props.reg_success === false ? (
-          <View>{this.successAlert()}</View>
-        ) : null}
-        <View style={styles.container}>
-          <View>
-            <View>
-              <TextInput
-                style={styles.inputText}
-                type="string"
-                required
-                placeholder="Full Name"
-                placeholderTextColor={Theme.colors.navyBlue}
-                onChangeText={name => this.setState({name, error1: false})}
-                value={this.state.name}
-              />
-            </View>
-            <View style={styles.error}>
-              {this.state.error1 ? <ErrorIcon /> : null}
-            </View>
-          </View>
-          {this.state.error1 ? (
-            <View style={{justifyContent: 'flex-end', marginRight: 5}}>
-              <Text
-                style={{
-                  color: 'white',
-                  fontSize: 0.04 * screenWidth,
-                  textAlign: 'center',
-                  padding: 2,
-                  marginTop: 2,
-                  marginBottom: 2,
-                  marginLeft: '30%',
-                  backgroundColor: 'black',
-                  borderTopWidth: 5,
-                  borderTopColor: 'red',
-                }}>
-                Please provide your name
-              </Text>
-            </View>
+        <ScrollView style ={{marginTop:40}} >
+          {this.props.loading ? <Spinner /> : null}
+          {this.props.error ? (
+            this.props.error.email ? (
+              <View>{this.emailAlert()}</View>
+            ) : this.props.error.mobile ? (
+              <View>{this.mobileAlert()}</View>
+            ) : null
           ) : null}
-         
-          <View>
-            <View
-              style={{
-                height: 65,
-                marginTop: 15,
-                marginLeft: 5,
-                marginRight: 5,
-                flexDirection: 'row',
-              }}>
-              <View
-                style={{
-                  marginRight: 3,
-                  flex: 0.2,
-                  backgroundColor: 'white',
-                  flexDirection: 'row',
-                  padding: 10,
-            
-                  alignItems: 'center',
-                }}>
-                <Image
-      
-                  resizeMode="contain"
-                  style={{
-                    width: 25,
-                    height: 25,
-                    padding: 5,
-                  }}
+          {this.props.reg_success === false ? (
+            <View>{this.successAlert()}</View>
+          ) : null}
+          <View style={styles.container}>
+            <View>
+              <View>
+
+                <Text
+                  style={styles.bigtext}
+                >
+                  Introcept Survey
+              </Text>
+              </View>
+            </View>
+            <View>
+              <View>
+
+                <TextInput
+                  style={styles.inputText}
+                  type="string"
+                  required
+                  placeholder="Full Name"
+                  placeholderTextColor={Theme.colors.navyBlue}
+                  onChangeText={name => this.setState({ name, error1: false })}
+                  value={this.state.name}
                 />
+              </View>
+              <View style={styles.error}>
+                {this.state.error1 ? <ErrorIcon /> : null}
+              </View>
+            </View>
+            {this.state.error1 ? (
+              <View style={{ justifyContent: 'flex-end', marginRight: 5 }}>
                 <Text
                   style={{
-                    fontSize: 18,
-                    color: Theme.colors.navyBlue,
+                    color: 'white',
+                    fontSize: 0.04 * screenWidth,
+                    textAlign: 'center',
+                    padding: 2,
+                    marginTop: 2,
+                    marginBottom: 2,
+                    marginLeft: '30%',
+                    backgroundColor: 'black',
+                    borderTopWidth: 5,
+                    borderTopColor: 'red',
                   }}>
-                  +977
-                </Text>
+                  Please provide your name
+              </Text>
               </View>
+            ) : null}
+
+            <View>
               <View
                 style={{
-                  flex: 0.8,
-                  backgroundColor: 'red',
-                  justifyContent: 'flex-start',
+                  height: 65,
+                  marginTop: 15,
+                  marginLeft: 5,
+                  marginRight: 5,
+                  flexDirection: 'row',
                 }}>
-                <TextInput
-                  autoCapitalize="none"
+                <View
                   style={{
-                    height: 65,
+                    marginRight: 3,
+                    flex: 0.4,
                     backgroundColor: 'white',
+                    flexDirection: 'row',
                     padding: 10,
-                    fontSize: 18,
-                    color: Theme.colors.navyBlue,
-                  }}
-                  type="string"
-                  keyboardType="numeric"
-                  required
-                  placeholder="Mobile Number"
-                  placeholderTextColor={Theme.colors.navyBlue}
-                  onChangeText={mobile_number =>
-                    this.setState({mobile_number, error3: false})
-                  }
-                  value={this.state.mobile_number}
-                />
+
+                    alignItems: 'center',
+                  }}>
+                  <Image
+
+                    resizeMode="contain"
+                    style={{
+                      width: 25,
+                      height: 25,
+                      padding: 5,
+                    }}
+                  />
+                  <Text
+                    style={{
+                      fontSize: 18,
+                      color: Theme.colors.navyBlue,
+                    }}>
+                    +977
+                </Text>
+                </View>
+                <View
+                  style={{
+                    flex: 0.6,
+                    backgroundColor: 'red',
+                    justifyContent: 'flex-start',
+                  }}>
+                  <TextInput
+                    autoCapitalize="none"
+                    style={{
+                      height: 65,
+                      backgroundColor: 'white',
+                      padding: 10,
+                      fontSize: 18,
+                      color: Theme.colors.navyBlue,
+                    }}
+                    type="string"
+                    keyboardType="numeric"
+                    required
+                    placeholder="Mobile Number"
+                    placeholderTextColor={Theme.colors.navyBlue}
+                    onChangeText={mobile_number =>
+                      this.setState({ mobile_number, error3: false })
+                    }
+                    value={this.state.mobile_number}
+                  />
+                </View>
+              </View>
+              <View style={styles.error}>
+                {this.state.error3 ? <ErrorIcon /> : null}
               </View>
             </View>
-            <View style={styles.error}>
-              {this.state.error3 ? <ErrorIcon /> : null}
-            </View>
-          </View>
-          {this.state.error3 ? (
-            <View style={{justifyContent: 'flex-end', marginRight: 5}}>
-              <Text
-                style={{
-                  color: 'white',
-                  fontSize: 0.04 * screenWidth,
-                  textAlign: 'center',
-                  padding: 2,
-                  marginBottom: 2,
-                  marginTop: 2,
+            {this.state.error3 ? (
+              <View style={{ justifyContent: 'flex-end', marginRight: 5 }}>
+                <Text
+                  style={{
+                    color: 'white',
+                    fontSize: 0.04 * screenWidth,
+                    textAlign: 'center',
+                    padding: 2,
+                    marginBottom: 2,
+                    marginTop: 2,
 
-                  marginLeft: '30%',
-                  backgroundColor: 'black',
-                  borderTopWidth: 4,
-                  borderTopColor: 'red',
-                }}>
-                Invalid mobile number.
+                    marginLeft: '30%',
+                    backgroundColor: 'black',
+                    borderTopWidth: 4,
+                    borderTopColor: 'red',
+                  }}>
+                  Invalid mobile number.
               </Text>
-            </View>
-          ) : null}
-          <View>
+              </View>
+            ) : null}
             <View>
-              <TextInput
-                autoCapitalize="none"
-                style={styles.inputText}
-                type="string"
-                required
-                placeholder="Brand of the Phone"
-                placeholderTextColor={Theme.colors.navyBlue}
-                onChangeText={password =>
-                  this.setState({password, error4: false})
-                }
-                value={this.state.password}
-              />
+              <View>
+                <TextInput
+                  autoCapitalize="none"
+                  style={styles.inputText}
+                  type="string"
+                  required
+                  placeholder="Brand of the Phone"
+                  placeholderTextColor={Theme.colors.navyBlue}
+                  onChangeText={password =>
+                    this.setState({ password, error4: false })
+                  }
+                  value={this.state.password}
+                />
+              </View>
+              <View style={styles.error}>
+                {this.state.error4 ? <ErrorIcon /> : null}
+              </View>
             </View>
-            <View style={styles.error}>
-              {this.state.error4 ? <ErrorIcon /> : null}
+            {this.state.error4 ? (
+              <View style={{ justifyContent: 'flex-end', marginRight: 5 }}>
+                <Text
+                  style={{
+                    color: 'white',
+                    fontSize: 0.04 * screenWidth,
+                    textAlign: 'center',
+                    padding: 2,
+                    marginTop: 2,
+                    marginBottom: 2,
+                    marginLeft: '30%',
+                    backgroundColor: 'black',
+                    borderTopWidth: 4,
+                    borderTopColor: 'red',
+                  }}>
+                  {this.state.password_error}
+                </Text>
+              </View>
+            ) : null}
+            <View
+              style={{
+                flexDirection: 'row',
+                marginTop: 5,
+                marginBottom: 10,
+
+              }}>
+
+
             </View>
-          </View>
-          {this.state.error4 ? (
-            <View style={{justifyContent: 'flex-end', marginRight: 5}}>
-              <Text
-                style={{
-                  color: 'white',
-                  fontSize: 0.04 * screenWidth,
-                  textAlign: 'center',
-                  padding: 2,
-                  marginTop: 2,
-                  marginBottom: 2,
-                  marginLeft: '30%',
-                  backgroundColor: 'black',
-                  borderTopWidth: 4,
-                  borderTopColor: 'red',
-                }}>
-                {this.state.password_error}
-              </Text>
-            </View>
-          ) : null}
-          <View
-            style={{
-              flexDirection: 'row',
-              marginTop: 5,
-              marginBottom: 10,
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() =>
+                this.onRegister(
+                  this.state.name,
+                  this.state.email,
+                  this.state.mobile_number,
+                  this.state.password,
+                  this.state.confirm_password,
+                  this.state.checked,
+                )
+              }>
+              <Text style={styles.text}>Save your Detail</Text>
+            </TouchableOpacity>
             
-            }}>
       
-          
+    
+            <Table style={{flexDirection: 'row'}} borderStyle={{borderWidth: 1}}>
+          {/* Left Wrapper */}
+          <TableWrapper style={{width: 80}}>
+            <Cell data="" style={styles.singleHead}/>
+            <TableWrapper style={{flexDirection: 'row'}}>
+              <Col data={['H1', 'H2']} style={styles.head} heightArr={[60, 60]} textStyle={styles.text} />
+              <Col data={this.state.tableTitle} style={styles.title} heightArr={[30, 30, 30, 30]} textStyle={styles.titleText}></Col>
+            </TableWrapper>
+          </TableWrapper>
+
+          {/* Right Wrapper */}
+          <TableWrapper style={{flex:1}}>
+            <Cols data={this.state.tableData} heightArr={[40, 30, 30, 30, 30]} textStyle={styles.text}/>
+          </TableWrapper>
+        </Table>
+
           </View>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() =>
-              this.onRegister(
-                this.state.name,
-                this.state.email,
-                this.state.mobile_number,
-                this.state.password,
-                this.state.confirm_password,
-                this.state.checked,
-              )
-            }>
-            <Text style={styles.text}>Save your Detail</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    </TouchableWithoutFeedback>
-  );
-   
+ 
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    );
+
   }
 }
 
@@ -258,14 +322,13 @@ const styles = StyleSheet.create({
     padding: 10,
     height: 65,
     flex: 10,
-    // borderWidth: 1,
     fontSize: 18,
     color: Theme.colors.navyBlue,
     backgroundColor: 'white',
-    marginTop: 15,
+    marginTop: 25,
     marginLeft: 5,
     marginRight: 5,
-    // marginBottom: 12
+
   },
   button: {
     justifyContent: 'center',
@@ -287,11 +350,27 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignSelf: 'flex-end',
   },
+  bigtext: {
+    fontSize: 25,
+    color: Theme.colors.navyBlue,
+    marginTop: 35,
+    textAlign: 'center',
+  },
+
+  tablecontainer: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+  singleHead: { width: 80, height: 40, backgroundColor: '#c8e1ff' },
+  head: { flex: 1, backgroundColor: '#c8e1ff' },
+  title: { flex: 2, backgroundColor: '#f6f8fa' },
+  titleText: { marginRight: 6, textAlign:'right' },
+  tabletext: { textAlign: 'center' },
+  tablebtn: { width: 58, height: 18, marginLeft: 15, backgroundColor: '#c8e1ff', borderRadius: 2 },
+  tablebtnText: { textAlign: 'center' }
+
 });
 
 const mapStateToProps = state => {
   return {};
 };
 export default connect(
-  
-  )(Home);
+
+)(Home);
--- app/component_scenes/action_types/HomeActionType.js
+++ app/component_scenes/action_types/HomeActionType.js
@@ -1,5 +1,3 @@
-
-
 export default (HomeActionType = {
     SAVE_TO_DATABASE_REQUEST: 'save_request',
     SAVE_TO_DATABASE_RESPONSE: 'save_success'
